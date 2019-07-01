e4e70ee3d079185207e52ec0305f6cdd5dd3fdff
Wilson Lam <wilson.j.lam@gmail.com>
2019-06-30 17:13:05 -0700

    rename page-2 to contact

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -4,13 +4,12 @@ import { Link } from "gatsby";
 import Layout from "../components/layout";
 import SEO from "../components/seo";
 
-const SecondPage = () => (
+const ContactPage = () => (
 	<Layout>
-		<SEO title="Page two" />
-		<h1>Hi from the second page</h1>
+		<SEO title="Contact" />
+		<h1>Hi from the Contact</h1>
 		<p>Welcome to page 2</p>
-		<Link to="/">Go back to the homepage</Link>
 	</Layout>
 );
 
-export default SecondPage;
+export default ContactPage;
